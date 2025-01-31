db49e80504c8270426f1b8cd85847b25868d7d08
Diogo Soares <diogoschsoares@gmail.com>
2025-01-31 11:22:34 +0000

    feat: Refactored adding a complete CRUD to comment using repository

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,117 @@
+using fin_application.Dtos.Comment;
+using fin_application.Helpers;
+using fin_application.Interfaces;
+using fin_application.Mappers;
+using fin_application.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fin_application.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+        private readonly IStockRepository _stockRepo;
+        private readonly IFMPService _fmpService;
+        public CommentController(ICommentRepository commentRepo,
+        IStockRepository stockRepo, UserManager<AppUser> userManager,
+        IFMPService fmpService)
+        {
+            _commentRepo = commentRepo;
+            _stockRepo = stockRepo;
+            _fmpService = fmpService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] CommentQueryObject queryObject)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var comments = await _commentRepo.GetAllAsync(queryObject);
+
+            var commentDto = comments.Select(s => s.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var comment = await _commentRepo.GetByIdAsync(id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
+
+        [HttpPost]
+        [Route("{symbol:alpha}")]
+        public async Task<IActionResult> Create([FromRoute] string symbol, CreateCommentDto commentDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var stock = await _stockRepo.GetBySymbolAsync(symbol);
+
+            if (stock == null)
+            {
+                stock = await _fmpService.FindStockBySymbolAsync(symbol);
+                if (stock == null)
+                {
+                    return BadRequest("Stock does not exists");
+                }
+                else
+                {
+                    await _stockRepo.CreateAsync(stock);
+                }
+            }
+
+            var commentModel = commentDto.ToCommentFromCreate(stock.Id);
+            await _commentRepo.CreateAsync(commentModel);
+            return CreatedAtAction(nameof(GetById), new { id = commentModel.Id }, commentModel.ToCommentDto());
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate(id));
+
+            if (comment == null)
+            {
+                return NotFound("Comment not found");
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var commentModel = await _commentRepo.DeleteAsync(id);
+
+            if (commentModel == null)
+            {
+                return NotFound("Comment does not exist");
+            }
+
+            return Ok(commentModel);
+        }
+    }
+}
